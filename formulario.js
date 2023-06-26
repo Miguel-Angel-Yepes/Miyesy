541e5b6935d3b4f747d8df64dff77bf4a2c60930
miguel <135150856+mrmiguelillo@users.noreply.github.com>
2023-06-26 08:12:44 -0500

    Add files via upload

---
+++ formulario.js
@@ -0,0 +1,47 @@
+function validarFormulario() {
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var telefono = document.getElementById("telefono").value;
+    var contrasena = document.getElementById("contrasena").value;
+    var confirmarContrasena = document.getElementById("confirmarContrasena").value;
+    
+    if (nombre === "") {
+        alert("Por favor, ingresa tu nombre.");
+        return false;
+    }
+    
+    var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!emailRegex.test(email)) {
+        alert("Por favor, ingresa un email válido.");
+        return false;
+    }
+    
+    var telefonoRegex = /^\d+$/;
+    if (!telefonoRegex.test(telefono)) {
+        alert("Por favor, ingresa un teléfono válido.");
+        return false;
+    }
+    
+    if (contrasena === "") {
+        alert("Por favor, ingresa una contraseña.");
+        return false;
+    }
+
+    if (contrasena.length < 5 ){
+        alert("La contraseña es muy corta");
+    return false;
+    }
+
+    if (contrasena.length > 15 ){
+        alert("La contraseña es muy larga");
+    return false;
+    }
+    
+    if (contrasena !== confirmarContrasena) {
+        alert("Las contraseñas no coinciden.");
+        return false;
+    }
+    
+    alert("¡Registro exitoso!");
+    return true;
+}
